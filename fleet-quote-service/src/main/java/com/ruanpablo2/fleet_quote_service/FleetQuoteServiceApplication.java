1bc36ec743b50a0203e461ffd75f20420025b39c
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-04-30 19:21:12 -0300

    commom, gateway and quote modules

---
+++ fleet-quote-service/src/main/java/com/ruanpablo2/fleet_quote_service/FleetQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruanpablo2.fleet_quote_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetQuoteServiceApplication.class, args);
+	}
+
+}
