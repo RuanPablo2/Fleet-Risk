e683cd9bb0a4bab6eae76ac17605bf20990decdb
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-05 21:42:07 -0300

    pricing-service created

---
+++ fleet-pricing-service/src/main/java/com/RuanPablo2/fleet_pricing_service/FleetPricingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.RuanPablo2.fleet_pricing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetPricingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetPricingServiceApplication.class, args);
+	}
+
+}
---
+++ fleet-pricing-service/src/test/java/com/RuanPablo2/fleet_pricing_service/FleetPricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RuanPablo2.fleet_pricing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetPricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
