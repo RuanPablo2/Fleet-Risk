13a231847fa0fce585d9fd764263e84c0789047a
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-11 19:52:30 -0300

    document service created and profiles configured

---
+++ fleet-document-service/src/main/java/com/RuanPablo2/fleet_document_service/FleetDocumentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.RuanPablo2.fleet_document_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetDocumentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetDocumentServiceApplication.class, args);
+	}
+
+}
