98104d31a02202a0c9490b99ada4e4541abe531e
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-13 19:15:17 -0300

    notification-service created

---
+++ fleet-notification-service/src/main/java/com/ruanpablo2/fleet_notification_service/FleetNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruanpablo2.fleet_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ fleet-notification-service/src/test/java/com/ruanpablo2/fleet_notification_service/FleetNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanpablo2.fleet_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
