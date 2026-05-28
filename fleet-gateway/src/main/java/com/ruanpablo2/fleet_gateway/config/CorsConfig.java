bbfae2b806f2dfdeed6a5becc3c3e79520b6c1d6
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-28 04:28:27 +0000

    add origin netlify no cors do gateway

--- fleet-gateway/src/main/java/com/ruanpablo2/fleet_gateway/config/CorsConfig.java
+++ fleet-gateway/src/main/java/com/ruanpablo2/fleet_gateway/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     public FilterRegistrationBean<CorsFilter> customCorsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://fleetrisk.netlify.app"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
