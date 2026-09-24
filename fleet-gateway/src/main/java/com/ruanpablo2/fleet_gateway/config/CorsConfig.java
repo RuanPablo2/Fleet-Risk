268547b60a73c54c6bbcecc2b29e98f710eb588f
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-09-23 22:16:23 -0300

    fix cors origins

--- fleet-gateway/src/main/java/com/ruanpablo2/fleet_gateway/config/CorsConfig.java
+++ fleet-gateway/src/main/java/com/ruanpablo2/fleet_gateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${CORS_ORIGINS:http://localhost:4200}")
+    @Value("${app.cors.origins}")
     private String corsOrigins;
 
     @Bean
